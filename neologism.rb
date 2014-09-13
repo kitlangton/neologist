9ab18775ddee7a56122b97675ff723edd47e37c5
Kit Langton <kitlangton@Kit-Langtons-iMac.local>
2014-09-13 11:03:11 -0400

    Remove suprefluous comments

--- neologism.rb
+++ neologism.rb
@@ -76,27 +76,3 @@ class Renamer
 
 
 end
-
-# d = Dir.new("#{Dir.home}/Desktop/Test Renamer/")
-# Dir.chdir(d)
-# d.each do |f|
-#   next unless f.include?("Screen")
-#   File.rename( f, new_name )
-#   p new_name
-# end
-
-#
-#
-# puts "Enter the file search query"
-# searchPattern = gets.strip
-# puts "Enter the target to replace"
-# target = gets.strip
-# puts "Enter the new target name"
-# newTarget = gets.strip
-# Dir.glob(searchPattern).sort.each do |entry|
-#   if File.basename(entry, File.extname(entry)).include?(target)
-#     newEntry = entry.gsub(target, newTarget)
-#     File.rename( entry, newEntry )
-#     puts "Rename from " + entry + " to " + newEntry
-#   end
-# end
